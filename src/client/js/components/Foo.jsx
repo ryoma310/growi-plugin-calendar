5c98e5a20cfdf28b5e94325cbc4385a904caaa51
ryoma310 <tirol-choco_6677@i.softbank.jp>
2021-02-27 04:44:17 +0900

    fix module name

--- src/client/js/components/Foo.jsx
+++ src/client/js/components/Foo.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import FooContext from 'growi-plugin-boilerplate/src/client/js/util/FooContext';
-import TagCacheManagerFactory from 'growi-plugin-boilerplate/src/client/js/util/TagCacheManagerFactory';
+import FooContext from 'growi-plugin-calendar/src/client/js/util/FooContext';
+import TagCacheManagerFactory from 'growi-plugin-calendar/src/client/js/util/TagCacheManagerFactory';
 
 // eslint-disable-next-line no-unused-vars
 import styles from '../../css/foo.css';
